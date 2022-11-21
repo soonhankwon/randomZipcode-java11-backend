d5bd77d0bcfb94a201ce9907211e018f80df2ab2
soonable@naver.com <soonable@naver.com>
2022-11-21 11:48:27 +0900

    modified

--- src/main/java/com/soonhankwon/randomzipcode/RandomZipcodeApplication.java
+++ src/main/java/com/soonhankwon/randomzipcode/RandomZipcodeApplication.java
@@ -3,11 +3,20 @@ package com.soonhankwon.randomzipcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class RandomZipcodeApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        for (int i = 0; i<number; i++) {
+            System.out.println("0" + ((int) (Math.random() * 196) + 8200));
+        }
         SpringApplication.run(RandomZipcodeApplication.class, args);
     }
-
 }
