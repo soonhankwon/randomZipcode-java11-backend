a4b4299f7073ca91f13403226bc4f70a884c8cd5
soonable@naver.com <soonable@naver.com>
2022-11-21 11:44:40 +0900

    Initial commit

---
+++ src/main/java/com/soonhankwon/randomzipcode/RandomZipcodeApplication.java
@@ -0,0 +1,13 @@
+package com.soonhankwon.randomzipcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomZipcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomZipcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soonhankwon/randomzipcode/RandomZipcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soonhankwon.randomzipcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomZipcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
