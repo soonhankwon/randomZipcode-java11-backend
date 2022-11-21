0ed458e6fd3c69d8f11548e6dd4df5035a77916f
soonable@naver.com <soonable@naver.com>
2022-11-21 12:25:21 +0900

    chore : modified for googlesheet

--- src/main/java/com/soonhankwon/randomzipcode/RandomZipcodeApplication.java
+++ src/main/java/com/soonhankwon/randomzipcode/RandomZipcodeApplication.java
@@ -11,12 +11,13 @@ import java.util.Scanner;
 public class RandomZipcodeApplication {
 
     public static void main(String[] args) throws IOException {
+//        SpringApplication.run(RandomZipcodeApplication.class, args);
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
 
         for (int i = 0; i<number; i++) {
-            System.out.println("0" + ((int) (Math.random() * 196) + 8200));
+//            System.out.println("0" + ((int) (Math.random() * 196) + 8200));
+            System.out.println("'0" + ((int) (Math.random() * 196) + 8200)); //구글 스프레드시트 용도
         }
-        SpringApplication.run(RandomZipcodeApplication.class, args);
     }
 }
